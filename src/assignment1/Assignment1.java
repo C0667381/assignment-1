264350d27c4f0c3b8f12794640e2c29dc8c5c7bc
simar chahal <simar chahal@10.99.5.172>
2015-09-16 03:07:19 -1200

    assignment 1 added!!!!!!!

---
+++ src/assignment1/Assignment1.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+/**
+ *
+ * @author simar chahal
+ */
+public class Assignment1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        // TODO code application logic here
+         /******
+         * 
+         * ASSIGNMENT-1
+         * 
+         * JOB POSTING
+         * http://www.indeed.ca/cmp/SportsDirect-Inc./jobs/Java-Developer-090e90fec5b52262?q=JAVA
+         * Java Developer 
+         * LOCATION :- Halifax, NS
+         * Job Type :- Full-time
+         * Experience required :- 3 years
+         * 
+         * 
+         * JAVA :- https://en.wikipedia.org/wiki/Java_(programming_language)
+         * .NET :- https://en.wikipedia.org/wiki/.NET_Framework
+         * C# :- https://en.wikipedia.org/wiki/C_Sharp_(programming_language)
+         * VB :- https://en.wikipedia.org/wiki/Visual_Basic
+         * HTML :- https://en.wikipedia.org/wiki/HTML
+         * XML :- https://en.wikipedia.org/wiki/XML
+         * SQL SERVER :- https://en.wikipedia.org/wiki/SQL_Server
+         * 
+         *Salary in 3 different location is given below:-
+         * 
+         * TORONTO :- $72k
+         * VANCUVER :- $76k
+         * WATERLOO :- $74k
+         *
+         */
+    }
+}
+    
+
